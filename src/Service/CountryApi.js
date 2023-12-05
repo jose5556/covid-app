2c0836d33a37ed05890547aa582323c277571f3d
jose <josefilmeneses@gmail.com>
2023-12-05 23:03:10 +0000

    countries and cases are now stored

--- src/Service/CountryApi.js
+++ src/Service/CountryApi.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function CountryApi() {
 
-    const covidApi = "https://disease.sh/v3/covid-19/countries"
+    const covidApi = "https://disease.sh/v3/covid-19/countries";
+    const [covidCountries, setCovidCountries] = useState([]);
+    const [cases, setCases] = useState([]);
+
 
     useEffect(() => {
         fetch(covidApi)
             .then((response) => response.json())
             .then((data) => {
-                data.forEach((country) => {
-                    console.log(country.country)
+                const country = []
+                const cases = []
+                data.forEach((apiData) => {
+                    country.push(apiData.country)
+                    cases.push(apiData.cases)
                 })
+                setCovidCountries(country);
+                setCases(cases);
             })
 
-    })
+            .catch((error) => {
+                console.error("Error in CountryApi:", error)
+            })
+
+    }, [covidApi])
     return (
         <div>CountryApi</div>
     )
