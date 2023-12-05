689ba58e9d9f372c9b29fbd0c19d915c1a79d365
jose <josefilmeneses@gmail.com>
2023-12-05 21:44:24 +0000

    map is on position

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,13 +7,9 @@ const Map = () => {
     const map = L.map('map').setView([51.505, -0.09], 13);
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      maxZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
-    
-    const marker = L.marker([51.5, -0.09]).addTo(map);
-    marker.bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
     
     return () => {
       map.remove();
@@ -21,7 +17,7 @@ const Map = () => {
   }, []);
 
   return (
-    <div id="map" style={{ width: '100%', height: '400px' }}></div>
+    <div className="map" id="map" style={{ width: '100%', height: '450px' }}></div>
   );
 };
 
