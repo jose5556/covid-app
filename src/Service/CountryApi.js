0c552449eeee9d54dc3712b7da0b86cd3d0e5700
jose <josefilmeneses@gmail.com>
2023-12-05 23:08:02 +0000

    flags are stored

--- src/Service/CountryApi.js
+++ src/Service/CountryApi.js
@@ -6,20 +6,25 @@ function CountryApi() {
     const covidApi = "https://disease.sh/v3/covid-19/countries";
     const [covidCountries, setCovidCountries] = useState([]);
     const [cases, setCases] = useState([]);
+    const [flags, setFlags] = useState([]);
 
 
     useEffect(() => {
         fetch(covidApi)
             .then((response) => response.json())
             .then((data) => {
-                const country = []
-                const cases = []
+                const country = [];
+                const cases = [];
+                const flag = [];
                 data.forEach((apiData) => {
                     country.push(apiData.country)
                     cases.push(apiData.cases)
+                    flag.push(apiData.countryInfo.flag)
+
                 })
                 setCovidCountries(country);
                 setCases(cases);
+                setFlags(flag)
             })
 
             .catch((error) => {
