04fd52186690fb40dcf1f43f0f75c78dd4d9fe07
jose <josefilmeneses@gmail.com>
2023-12-10 16:30:14 +0000

    flags are now in the right side

--- src/components/CovidNavbar.js
+++ src/components/CovidNavbar.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
 import '../App.css';
 
 function CovidNavbar({ countriesData }) {
-
-  if (!countriesData || countriesData.length < 0) {
-    return <div>Nothing here my friend</div>;
+  if (!countriesData || countriesData.length === 0) {
+    return <div>Nothing here, my friend</div>;
   }
 
   return (
@@ -12,17 +11,27 @@ function CovidNavbar({ countriesData }) {
       <div>{countriesData.reduce((total, data) => total + data.cases, 0)}</div>
 
       {countriesData.map((data, index) => (
-        <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-          <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
-          <img src={data.flag} alt={`Flag ${index}`} style={{ width: '23px', height: 'auto' }} />
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '5px', 
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
+          </div>
+          <img
+            src={data.flag}
+            alt={`Flag ${index}`}
+            style={{ width: '23px', height: 'auto' }}
+          />
         </div>
       ))}
     </div>
-  )
-
+  );
 }
 
-export default CovidNavbar
-
-
-
+export default CovidNavbar;
