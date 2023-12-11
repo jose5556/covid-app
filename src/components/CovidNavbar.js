1e64c572c40647ecdaafc16454cecc87132b5736
jose <josefilmeneses@gmail.com>
2023-12-11 15:16:44 +0000

    button && input in front-end

--- src/components/CovidNavbar.js
+++ src/components/CovidNavbar.js
@@ -3,34 +3,41 @@ import '../App.css';
 
 function CovidNavbar({ countriesData }) {
   if (!countriesData || countriesData.length === 0) {
-    return <div>Nothing here, my friend</div>;
+    return;
   }
 
   return (
     <div>
       <div>{countriesData.reduce((total, data) => total + data.cases, 0)}</div>
 
-      {countriesData.map((data, index) => (
-        <div
-          key={index}
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: '5px', 
-          }}
-        >
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
+      <div class="input-container">
+        <button class="square-button">up</button>
+        <input type="text" class="input-field" placeholder="Search for the country"></input>
+      </div>
+
+      {
+        countriesData.map((data, index) => (
+          <div
+            key={index}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: '5px',
+            }}
+          >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
+            </div>
+            <img
+              src={data.flag}
+              alt={`Flag ${index}`}
+              style={{ width: '23px', height: 'auto' }}
+            />
           </div>
-          <img
-            src={data.flag}
-            alt={`Flag ${index}`}
-            style={{ width: '23px', height: 'auto' }}
-          />
-        </div>
-      ))}
-    </div>
+        ))
+      }
+    </div >
   );
 }
 
