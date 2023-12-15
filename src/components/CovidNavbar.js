b47584ac57e65c21b4d09760cad27aa973f38de1
jose <josefilmeneses@gmail.com>
2023-12-15 11:36:30 +0000

    input now serch the country

--- src/components/CovidNavbar.js
+++ src/components/CovidNavbar.js
@@ -1,48 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 function CovidNavbar({ countriesData }) {
+  const [input, setInput] = useState("");
+  
   if (!countriesData || countriesData.length === 0) {
     return;
   }
 
+  const handleChange = e => {
+    setInput(e.target.value);
+    console.log(setInput)
+  };
+
+  let filteredCountries = countriesData;
+
+  if (input.trim() !== "") {
+    filteredCountries = countriesData.filter(data =>
+      data.country.toLowerCase().includes(input.toLowerCase())
+    );
+  }
+
   return (
     <div>
-      <div
-        style={{ textAlign: 'center', marginBottom: '10px' }}> 
-        {countriesData.reduce((total, data) => total + data.cases, 0)}
+      <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+        {filteredCountries.reduce((total, data) => total + data.cases, 0)}
       </div>
 
-
-      <div class="input-container">
-        <button class="square-button">up</button>
-        <input type="text" class="input-field" placeholder="Search..."></input>
+      <div className="input-container">
+        <button className="square-button">up</button>
+        <input
+          type="text"
+          className="input-field"
+          value={input}
+          onChange={handleChange}
+          placeholder="Search..."
+        />
       </div>
 
-      {
-        countriesData.map((data, index) => (
-          <div
-            key={index}
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginBottom: '5px',
-            }}
-          >
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
-            </div>
-            <img
-              src={data.flag}
-              alt={`Flag ${index}`}
-              style={{ width: '23px', height: 'auto' }}
-            />
+      {filteredCountries.map((data, index) => (
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '5px',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p style={{ marginRight: '10px' }}>{data.cases} in {data.country}</p>
           </div>
-        ))
-      }
-    </div >
+          <img
+            src={data.flag}
+            alt={`Flag ${index}`}
+            style={{ width: '23px', height: 'auto' }}
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
 export default CovidNavbar;
+
+
